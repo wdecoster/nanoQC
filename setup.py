3511cf910c99bd90946edacfafc7743956d38282
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-07-31 18:49:39 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+exec(open('nanoQC/version.py').read())
+
+setup(
+    name='NanoQC',
+    version=__version__,
+    description='Create fastQC-like plots for Oxford Nanopore sequencing data',
+    long_description=open(path.join(here, "README.rst")).read(),
+    url='https://github.com/wdecoster/nanoQC',
+    author='Wouter De Coster',
+    author_email='decosterwouter@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='nanopore sequencing statistics',
+    packages=find_packages(),
+    install_requires=[
+        'seaborn',
+        'numpy',
+        'matplotlib>=2.0.0'
+        'biopython'
+        ],
+    package_data={'NanoStat': []},
+    package_dir={'nanostat': 'nanostat'},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'NanoQC=nanoQC.NanoQC:main',
+        ],
+    },
+)
