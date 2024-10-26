5f254bc76a4cd1aac80696462e50807c62319385
wdecoster <decosterwouter@gmail.com>
2024-10-26 22:13:53 +0200

    major change, use plotly instead of bokeh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,11 @@
 set -ev
 
-git clone https://github.com/wdecoster/nanotest.git
+if [ -d "nanotest" ]; then
+    echo "nanotest already cloned"
+else
+    git clone https://github.com/wdecoster/nanotest.git
+fi
 
 nanoQC -h
 nanoQC nanotest/reads.fastq.gz
-nanoQC nanotest/reads.fastq.gz --minlen 100
+nanoQC nanotest/reads.fastq.gz --minlen 500
